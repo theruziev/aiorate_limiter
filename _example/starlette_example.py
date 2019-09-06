2c1d33b0ce67a11e3736c8eb5b5d469da1035c33
Bakhtiyor Ruziev <bakhtiyor.ruziev@yandex.ru>
2019-09-06 15:15:13 +0300

    rewrite starlette example

--- _example/starlette_example.py
+++ _example/starlette_example.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from starlette.applications import Starlette
 from starlette.datastructures import MutableHeaders
 from starlette.requests import Request
@@ -5,6 +7,7 @@ from starlette.responses import JSONResponse
 import uvicorn
 from starlette.types import ASGIApp, Scope, Receive, Send, Message
 
+from aiorate_limiter import RateLimiterResult
 from aiorate_limiter.base import RateLimiterOpts
 from aiorate_limiter.storage.memory import MemoryRateLimiter
 
@@ -13,32 +16,39 @@ app = Starlette(debug=True)
 limiter = MemoryRateLimiter(RateLimiterOpts(points=2, duration=5000))
 
 
+async def unattached_send(message: Message) -> None:
+    raise RuntimeError("send awaitable not set")  # pragma: no cover
+
+
 class RateLimiterMiddleware:
     def __init__(self, app: ASGIApp) -> None:
         self.app = app
+        self.send = unattached_send
+        self.rl_res = None
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] != "http":
             await self.app(scope, receive, send)
             return
-
+        self.send = send
         request = Request(scope, receive)
-
         res = await limiter.consume(request.client.host)
-
-        async def send_wrapper(msg: Message):
-            if msg["type"] == "http.response.start":
-                headers = MutableHeaders(scope=msg)
-                headers.append("X-Rate-Limit-Limit", str(res.consumed_points))
-                headers.append("X-Rate-Limit-Remaining", str(res.remaining_points))
-                headers.append("X-Rate-Limit-Reset", str(res.ms_before_next))
-            await send(msg)
-
+        self.rl_res = res
         if res.is_allowed:
-            await self.app(scope, receive, send_wrapper)
+            await self.app(scope, receive, self.send_wrapper)
             return
+
         response = JSONResponse({"error": "To many requests"}, status_code=429)
-        await response(scope, receive, send_wrapper)
+        await response(scope, receive, self.send_wrapper)
+
+    async def send_wrapper(self, msg: Message):
+        if msg["type"] == "http.response.start":
+            result = self.rl_res
+            headers = MutableHeaders(scope=msg)
+            headers.append("X-Rate-Limit-Limit", str(result.consumed_points))
+            headers.append("X-Rate-Limit-Remaining", str(result.remaining_points))
+            headers.append("X-Rate-Limit-Reset", str(result.ms_before_next))
+        await self.send(msg)
 
 
 @app.route("/")
